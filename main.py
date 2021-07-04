1153c69cc9958853c97b6080ee086f9f6fe1aa0b
Bilal K. Hamada <bilalkamal2009@gmail.com>
2021-07-04 14:09:19 +0200

    UI

--- main.py
+++ main.py
@@ -16,3 +16,28 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx = 100, pady = 50, bg = YELLOW)
+window.minsize(400, 300)
+
+timer_label = Label(text = "Timer", bg = YELLOW, fg = GREEN, font = (FONT_NAME, 40, "bold"))
+timer_label.grid(row = 0, column = 1)
+
+canvas = Canvas(width=200, height = 224, bg = YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file = "tomato.png")
+canvas.create_image(100, 112 , image=tomato_img)
+canvas.create_text(100, 130, text = "00:00", fill = "white", font = (FONT_NAME, 35, "bold"))
+canvas.grid(row = 1, column= 1)
+
+start_btn = Button(text= "Start", highlightthickness=0)
+start_btn.grid(row = 2, column = 0)
+reset_bt = Button(text= "Reset", highlightthickness=0)
+reset_bt.grid(row = 2, column = 2)
+
+progress_label = Label(text = "✔",bg = YELLOW, fg= GREEN )
+progress_label.grid(row =3, column = 1)
+
+
+window.mainloop()
